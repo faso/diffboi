6d184a2c1968e3e46a4a7bac11df37a05c0a3a11
Arthur Tabatchnic <arthur.tabatchnic@innoforce.com>
2018-01-03 12:31:33 +0200

    first commit 2

---
+++ MegaDiffBoi/Migrations/20180103094951_ionitial.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MegaDiffBoi.Migrations
+{
+    public partial class ionitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Paragraphs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Page = table.Column<string>(nullable: true),
+                    Query = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paragraphs", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Entries",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ParagraphID = table.Column<int>(nullable: false),
+                    Timestamp = table.Column<DateTime>(nullable: false),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entries", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Entries_Paragraphs_ParagraphID",
+                        column: x => x.ParagraphID,
+                        principalTable: "Paragraphs",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entries_ParagraphID",
+                table: "Entries",
+                column: "ParagraphID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entries");
+
+            migrationBuilder.DropTable(
+                name: "Paragraphs");
+        }
+    }
+}
---
+++ MegaDiffBoi/Models/DiffBoiContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace MegaDiffBoi.Models
+{
+    public class DiffBoiContext : DbContext
+    {
+        public DiffBoiContext(DbContextOptions<DiffBoiContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Paragraph> Paragraphs { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+
+    public class Paragraph
+    {
+        public int ID { get; set; }
+        public string Page { get; set; }
+        public string Query { get; set; }
+    }
+
+    public class Entry
+    {
+        public int ID { get; set; }
+        public int ParagraphID { get; set; }
+        public Paragraph Paragraph { get; set; }
+
+        public string Value { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
